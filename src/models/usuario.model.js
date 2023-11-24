4948a48b756ed0a96d901a8f4a8ceda55a5a2671
adri-23 <adrianabermudez1023@gmail.com>
2023-11-23 21:11:27 -0600

    se impletendo insertar usuario

--- src/models/usuario.model.js
+++ src/models/usuario.model.js
@@ -7,11 +7,45 @@ const usuarioModel = {
       const query =
         "SELECT * FROM cat_usuario WHERE NOMBRE = ? AND CONTRASENA = ?";
       const [rows, fields] = await pool.execute(query, [NOMBRE, CONTRASENA]);
-      pool.end();
+      //pool.end();
       return rows;
     } catch (err) {
       console.error("Error al realizar el login", err);
       throw new Error("Error al realizar el login");
+    }
+  },
+
+  insert: async (user) => {
+    try {
+      const query =
+        "CALL pa_insert_Usuario(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+      const values = [
+        user.ID_USUARIO,
+        user.NOMBRE,
+        user.AP_PATERNO,
+        user.AP_MATERNO,
+        user.CURP,
+        user.RFC,
+        user.FECHA_NACIMIENTO,
+        user.TELEFONO,
+        user.EMAIL,
+        user.USUARIO,
+        user.CONTRASENA,
+        user.ESTATUS,
+        user.FECHA_ALTA,
+        user.FECHA_BAJA,
+        user.ID_ALTA,
+        user.ID_BAJA,
+        user.FECHA_VIGENCIA,
+        user.ID_PERFIL,
+      ];
+      const [rows] = await pool.execute(query, values);
+      pool.end();
+      return rows;
+    } catch (err) {
+      console.log(err);
+      return [];
+    } finally {
     }
   },
 };
--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -3,5 +3,6 @@ import express from "express";
 const router = express.Router();
 
 router.post("/login", usuarioCtrl.loginUsuario);
+router.post("/usuarios", usuarioCtrl.createUsuario);
 
 export default router;
